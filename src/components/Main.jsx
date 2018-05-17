2712b69f0cee3915448284b3ba8cd54ed18fa5f9
mateus alves <theuves@gmail.com>
2018-05-16 21:49:17 -0400

    adiciona tabuleiro e funcionamento básico
    - adiciona table
    - manipula a seleção do jogador

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,11 +6,26 @@ import velha from "theuves-velha";
 // others
 import actions from "../actions.js";
 
+import Table from "./Table/index.jsx";
+
 class Main extends Component {
+  selectCell = (index) => {
+    const {current} = this.props.state.players;
+    const player = this.props.state.players.symbol[current];
+
+    this.props.play(player, index);
+    this.props.addCell(index);
+  }
   render() {
     return (
       <div>
-
+        <Table
+          size={300}
+          borders={3}
+          color="black"
+          cells={this.props.state.cells}
+          onSelectCell={this.selectCell}
+        />
       </div>
     );
   }
