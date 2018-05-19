962dfa8a7e8c775cbaa2c0de8f147448447a1853
mateus alves <theuves@gmail.com>
2018-05-19 14:34:10 -0400

    adiciona componente: Alert
    - apresentará o botão de reiniciar o jogo
    - apresentará quem venceu a jogada

---
+++ src/components/Alert/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: #34495e;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+function Alert() {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default Alert;
