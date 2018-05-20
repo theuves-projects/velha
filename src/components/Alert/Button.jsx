390ccb007f69aebfb5c2a72049065817665e8ff1
mateus alves <theuves@gmail.com>
2018-05-20 16:11:08 -0400

    Alert/Button: adiciona efeitos

--- src/components/Alert/Button.jsx
+++ src/components/Alert/Button.jsx
@@ -6,12 +6,25 @@ const Button = styled.button`
   font-size: var(--size);
   width: var(--size);
   height: var(--size);
-  transform: translateY(-50%);
   border-radius: 100px;
   border: none;
   outline: none;
   background-color: #e67e22;
   color: white;
+
+  transform: translateY(-50%);
+  filter: contrast(1);
+  transition: all .15s;
+
+  &:hover {
+    transform: translateY(-50%) scale(1.15);
+    filter: contrast(1.05);
+  }
+
+  &:active {
+    transform: translateY(-50%) scale(1.1);
+    filter: contrast(.95);
+  }
 `;
 
 function This() {
