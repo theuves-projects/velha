ce0424e11500f885d901498f5e4dd1e938f5dd77
Matheus Alves <theuves@gmail.com>
2019-01-08 22:34:52 -0300

    Add App container

---
+++ src/containers/App.js
@@ -0,0 +1,38 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../actions/game'
+import { bindActionCreators } from 'redux'
+import Alert from '../components/Alert'
+import Player from '../components/Player'
+import Scoreboard from '../components/Scoreboard'
+import Table from '../components/Table'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <Fragment>
+        
+      </Fragment>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    game: state.game
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
