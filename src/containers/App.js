5d8cf062b8b353d2cd5f60ea87cf115125e6c549
Matheus Alves <theuves@gmail.com>
2019-01-09 17:10:52 -0300

    Add more functions

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,18 +7,36 @@ import Scoreboard from '../components/Scoreboard'
 import Board from '../components/Board'
 import Alert from '../components/Alert'
 
-const StyledScoreboardLeft = styled.div`
+const StyledScoreboardX = styled.div`
   position: absolute;
   top: 45%;
   left: 0;
   transform: translateY(-50%);
+
+  &::after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 `
 
-const StyledScoreboardRight = styled.div`
+const StyledScoreboardO = styled.div`
   position: absolute;
   top: 45%;
   right: 0;
   transform: translateY(-50%);
+
+  &::after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 `
 
 const StyledBoard = styled.div`
@@ -33,39 +51,70 @@ class App extends Component {
     if (this.props.game.isFinished) return
     if (this.props.game.board[index]) return window.alert('Invalid!')
 
+    console.log(this.props.game.players)
+
+    // For the human.
     this.props.actions.play({
       index,
-      player: this.props.game.mainPlayer
+      player: this.props.game.players.human.name
     })
 
+    // For the computer.
     setTimeout(() => {
-      this.props.actions.autoPlay({
-        player: this.props.game.mainPlayer === 'x'
-          ? 'o'
-          : 'x'
+      this.props.actions.play({
+        player: this.props.game.players.human.name === 'x' ? 'o' : 'x'
       })
     }, 250)
   }
-  render() {
-    const { board, isFinished, winner } = this.props.game
+  togglePlayer = (name) => {
+    if (this.props.game.players.human.name === name) return
 
+    if (!this.props.game.board.every((value) => value === null)) {
+      return window.alert('Finish the game before.')
+    }
+
+    this.props.actions.togglePlayer()
+  }
+  start = () => {
+    this.props.actions.restart()
+  }
+  restart = () => {
+    if (this.props.game.board.every((value) => value === null)) {
+      return
+    }
+
+    if (window.confirm('Are you sure?')) {
+      this.props.actions.restart()
+    }
+  }
+  render() {
     return (
       <Fragment>
-        <StyledScoreboardRight>
+        <StyledScoreboardX onClick={() => this.togglePlayer('x')}>
           <Scoreboard
             width={50}
             playerName='x'
+            playerColor={this.props.game.players.human.name === 'x' ? '#8bc34a' : undefined}
+            score={this.props.game.players.human.name === 'x'
+              ? this.props.game.players.human.victories
+              : this.props.game.players.computer.victories
+            }
           />
-        </StyledScoreboardRight>
-        <StyledScoreboardLeft>
+        </StyledScoreboardX>
+        <StyledScoreboardO onClick={() => this.togglePlayer('o')}>
           <Scoreboard
             width={50}
             playerName='o'
+            playerColor={this.props.game.players.human.name === 'o' ? '#8bc34a' : undefined}
+            score={this.props.game.players.human.name === 'o'
+              ? this.props.game.players.human.victories
+              : this.props.game.players.computer.victories
+            }
           />
-        </StyledScoreboardLeft>
+        </StyledScoreboardO>
         <StyledBoard>
           <Board
-            board={board}
+            board={this.props.game.board}
             onClick={this.play}
             cellWidth={110}
             cellColor='white'
@@ -74,11 +123,14 @@ class App extends Component {
           />
         </StyledBoard>
         <Alert
-          isOpen={isFinished}
-          winner={winner}
+          isOpen={this.props.game.isFinished}
+          onClickButtonOpened={this.start}
+          onClickButtonClosed={this.restart}
+          winner={this.props.game.winner}
           containerHeight={10}
           buttonWidth={65}
-          buttonIcon='refresh'
+          buttonIconOpened='play_arrow'
+          buttonIconClosed='refresh'
         />
       </Fragment>
     )
