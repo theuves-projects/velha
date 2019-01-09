fe4ecaf9695b737bc34a23eed7944dcbb499d4a0
Matheus Alves <theuves@gmail.com>
2019-01-08 21:35:44 -0300

    Add actions

---
+++ src/actions/game.js
@@ -0,0 +1,5 @@
+import { createActions } from 'redux-actions'
+
+export const finish = createAction('finish')
+export const play = createAction('play')
+export const autoPlay = createAction('auto play')
