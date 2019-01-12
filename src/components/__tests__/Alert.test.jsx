ec9de485601d70cec4728796c89c04f10d6b1a66
Matheus Alves <theuves@gmail.com>
2019-01-12 09:18:29 -0300

    Add more a test for the Alert component

--- src/components/__tests__/Alert.test.jsx
+++ src/components/__tests__/Alert.test.jsx
@@ -10,15 +10,34 @@ import Player from '../Player.jsx'
 
 describe('<Alert />', () => {
   test('should have two Player component', () => {
-    const wrapper = Enzyme.mount(<Alert buttonColor='black' />)
+    const wrapper = Enzyme.mount(
+      <Alert buttonColor='black' />
+    )
     expect(wrapper.find(Player)).toHaveLength(2)
   })
   test('should have one Player component', () => {
-    const wrapper = Enzyme.mount(<Alert buttonColor='black' winner='x' />)
+    const wrapper = Enzyme.mount(
+      <Alert
+        buttonColor='black'
+        winner='x'
+      />
+    )
     expect(wrapper.find(Player)).toHaveLength(1)
   })
-  test('should renderize the button icon', () => {
-    const wrapper = Enzyme.render(<Alert buttonColor='black' buttonIconClosed='foo' />)
+  test('should renderize the button icon whe the alert is closed', () => {
+    const wrapper = Enzyme.render(<Alert
+      buttonColor='black'
+      buttonIconClosed='foo'
+    />)
     expect(wrapper.find('.material-icons').text()).toBe('foo')
+  })
+  test('should renderize the button icon whe the alert is opened', () => {
+    const wrapper = Enzyme.render(<Alert
+      buttonColor='black'
+      isOpen={true}
+      buttonIconOpened='bar'
+      buttonIconClosed='foo'
+    />)
+    expect(wrapper.find('.material-icons').text()).toBe('bar')
   })
 })
